1f8853d2804535643766473bb7a18377b4a4b0cf
LekKit <50500857+LekKit@users.noreply.github.com>
2026-01-21 15:07:14 +0200

    rustc_target: Introduce riscv64gc-unknown-haiku

--- compiler/rustc_target/src/spec/base/haiku.rs
+++ compiler/rustc_target/src/spec/base/haiku.rs
@@ -1,11 +1,17 @@
-use crate::spec::{RelroLevel, TargetOptions, cvs};
+use crate::spec::{TlsModel, RelroLevel, StackProbeType, TargetOptions, cvs};
 
 pub(crate) fn opts() -> TargetOptions {
     TargetOptions {
         os: "haiku".into(),
-        dynamic_linking: true,
         families: cvs!["unix"],
+        has_rpath: true,
+        dynamic_linking: true,
+        plt_by_default: false,
+        has_thread_local: true,
+        position_independent_executables: true,
+        tls_model: TlsModel::GeneralDynamic,
         relro_level: RelroLevel::Full,
+        stack_probes: StackProbeType::Inline,
         ..Default::default()
     }
 }
